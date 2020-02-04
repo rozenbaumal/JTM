059fc093c7ed4409f1fb000d3afd7ab82dbdd064
rozenbaumal <rozenbauma.l@gmail.com>
2020-02-04 18:53:13 +0000

    changed the greeting

--- src/main/java/jtm/activity02/Hello.java
+++ src/main/java/jtm/activity02/Hello.java
@@ -3,6 +3,6 @@ package jtm.activity02;
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Java!");
+        System.out.println("Hello world!");
     }
 }
